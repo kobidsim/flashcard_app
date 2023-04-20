76cb37edd584bf0e562ba04594dee10b88c8d65b
Kobid Simkhada <simkhadakobid@gmail.com>
2023-04-20 16:14:33 +0545

    Added state management for decks. Completed the very first page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => DeckData(), child: const MyApp()));
 }
 
-//user data
-List<String> decks = <String>["hyatteri 1", "Deck 2", "Deck 3"];
+//user data provider
+class DeckData extends ChangeNotifier {
+  final List<String> _decks = <String>[];
+
+  UnmodifiableListView<String> get items => UnmodifiableListView(_decks);
+
+  int get noOfDecks => _decks.length;
+
+  void addToDeck(String deck) {
+    _decks.add(deck);
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -30,41 +45,80 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
-        backgroundColor: theme.colorScheme.background,
-        appBar: AppBar(
-          title: const Text('My Decks'),
-        ),
-        body: DeckList(decks: decks),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Add Deck',
-          child: const Icon(Icons.add),
-        ));
+      backgroundColor: theme.colorScheme.background,
+      appBar: AppBar(
+        title: const Text('My Decks'),
+      ),
+      body: const DeckList(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AddDeckButton(),
+    );
   }
 }
 
 class DeckList extends StatelessWidget {
-  const DeckList({required this.decks, super.key});
-
-  final List<String> decks;
+  const DeckList({super.key});
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
-    return ListView.builder(
-        itemCount: decks.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            color: theme.colorScheme.surface,
-            child: ListTile(
-              title: Text(
-                decks[index],
-                style: TextStyle(color: theme.colorScheme.onSurface),
+    return Consumer<DeckData>(builder: (context, decks, child) {
+      return ListView.builder(
+          itemCount: decks.noOfDecks,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              color: theme.colorScheme.surface,
+              child: ListTile(
+                leading: const Icon(Icons.book),
+                title: Text(
+                  decks.items[index],
+                  style: TextStyle(color: theme.colorScheme.onSurface),
+                ),
               ),
-            ),
-          );
-        });
+            );
+          });
+    });
+  }
+}
+
+class AddDeckButton extends StatelessWidget {
+  AddDeckButton({super.key});
+  final inputController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+            barrierDismissible: true,
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Name:"),
+                content: TextField(
+                  autofocus: true,
+                  controller: inputController,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter name of your Deck'),
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        if (inputController.text != "") {
+                          Provider.of<DeckData>(context, listen: false)
+                              .addToDeck(inputController.text);
+                          inputController.text = "";
+                        }
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("OK"))
+                ],
+              );
+            });
+      },
+      tooltip: 'Add Deck',
+      child: const Icon(Icons.add),
+    );
   }
 }
